7a4218ee6eb18f0c8eb5c5a89361d7d54cf16c21
缘缘 <1253301988@qq.com>
2022-12-09 23:59:46 +0800

    公共UI: 添加属性按钮

---
+++ Unity/Codes/HotfixView/Demo/UI/Common/ES_AddAttributeSystem.cs
@@ -0,0 +1,37 @@
+﻿namespace ET
+{
+    public static class ES_AddAttributeSystem
+    {
+        public static void RegisterNumericType(this ES_AddAttribute self, int numericType)
+        {
+            self.EB_AddAttributeButton.AddListener(() => self.OnAddAttribute(numericType));
+        }
+
+        public static void Refresh(this ES_AddAttribute self, int numericType, long modify = 0)
+        {
+            var numericComponent = UnitHelper.GetMyUnitNumericComponentFromCurScene(self.ZoneScene().CurrentScene());
+            var value = numericComponent.GetAsLong(numericType);
+            PlayerNumericConfig config = PlayerNumericConfigCategory.Instance.Get(numericType);
+            self.ET_AttributeNameTextMeshProUGUI.text = $"{config.Name}: ";
+            if (modify != 0)
+                self.ET_AttributeValueTextMeshProUGUI.text = $"<color=green>{value + modify:d4}</color>";
+            else
+                self.ET_AttributeValueTextMeshProUGUI.text = $"{value:d4}";
+        }
+
+        private static void OnAddAttribute(this ES_AddAttribute self, int numericType)
+        {
+            var numericComponent = UnitHelper.GetMyUnitNumericComponentFromCurScene(self.ZoneScene().CurrentScene());
+            var points = numericComponent.GetAsLong(NumericType.AttributePoints);
+            if (points > 0)
+            {
+                Game.EventSystem.Publish(new EventType.AddAttribute()
+                {
+                    ZoneScene = self.ZoneScene(),
+                    NumericType = numericType,
+                    AddValue = 1
+                });
+            }
+        }
+    }
+}
---
+++ Unity/Codes/HotfixView/Demo/UIBehaviour/CommonUI/ES_AddAttributeViewSystem.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET
+{
+	[ObjectSystem]
+	public class ES_AddAttributeAwakeSystem : AwakeSystem<ES_AddAttribute,Transform> 
+	{
+		public override void Awake(ES_AddAttribute self,Transform transform)
+		{
+			self.uiTransform = transform;
+		}
+	}
+
+
+	[ObjectSystem]
+	public class ES_AddAttributeDestroySystem : DestroySystem<ES_AddAttribute> 
+	{
+		public override void Destroy(ES_AddAttribute self)
+		{
+			self.DestroyWidget();
+		}
+	}
+}
