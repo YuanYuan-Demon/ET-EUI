56499f5f603c1d7b84fc104673cf4785a93ce276
缘缘 <1253301988@qq.com>
2023-03-25 12:22:30 +0800

    导入资源

---
+++ Unity/Assets/Scripts/Loader/MonoBehaviour/DrawRaycastUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET
+{
+    public class DrawRaycastUI : MonoBehaviour
+    {
+        private static Vector3[] fourCorners = new Vector3[4];
+
+        private void OnDrawGizmos()
+        {
+            foreach (var g in FindObjectsOfType<MaskableGraphic>())
+            {
+                if (g.raycastTarget)
+                {
+                    RectTransform rectTransform = g.transform as RectTransform;
+                    rectTransform.GetWorldCorners(fourCorners);
+                    Gizmos.color = Color.green;
+                    for (int i = 0; i < 4; i++)
+                        Gizmos.DrawLine(fourCorners[i], fourCorners[(i + 1) % 4]);
+                }
+            }
+        }
+    }
+}
